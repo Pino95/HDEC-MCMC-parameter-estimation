82e0251a34c4055ecec9d04a13408b5eac340f07
Matteo Piani <97957512+Pino95@users.noreply.github.com>
2025-08-05 14:15:40 +0100

    Create model.py

---
+++ model.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+#Number of free parameters in the conformal factor
+nC=1
+
+#Number of free parameters in the kinetic function
+nK=1
+
+#Number of free parameter in the potential
+nV=0
+
+def Omega ():
+  return lambda x:
+
+def DOmega ():
+  return lambda x:
+
+def DDOmega ():
+  return lambda x:
+
+def K ():
+  return lambda x:
+
+def DK ():
+  return lambda x:
+
+def DDK ():
+  return lambda x:
+
+def V ():
+  return lambda x:
+
+def DV ():
+  return lambda x:
+
+def DDV ():
+  return lambda x:
